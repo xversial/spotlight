95e9891939255c4a4c60cea6bad7cf15183ba71e
Rémy J <remy@remyj.fr>
2017-05-17 16:43:25 +0200

    Updating spotlight.sh with adding new features: - Check internet connection before getting new picture - Keep all pictures (not replacing old picutres) - Change the target of the script: it now replace screensaver and no longer background Updating README.md to match with the new version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# spotlight
-Windows 10 Spotlight Background images for Gnome
+# Spotlight
+Windows 10 Spotlight Screensaver images for Gnome
+
+##Installation
+
+1. Clone the projet:
+```bash
+sudo git clone https://github.com/remyj38/spotlight.git /opt/spotlight
+```
+2. Create links for scripts:
+```bash
+sudo ln -s /opt/spotlight/spotlight.{service,timer} /etc/systemd/system/
+sudo ln -s /opt/spotlight/spotlight.sh /usr/bin/
+```
+3. Change the target user in the `spotlight.service` file
+4. Enable and start the service and the timer:
+```bash
+sudo systemctl deamon-reload
+sudo systemctl enable spotlight.service spotlight.timer
+sudo systemctl start spotlight.service
+```
+
--- spotlight.sh
+++ spotlight.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
 
-item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=en,en-US&ctry=US" | jq -r ".batchrsp.items | .[0].item")
-
+path="$HOME/.spotlight/"
+item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=fr,fr-FR&ctry=FR" | jq -r ".batchrsp.items | .[0].item")
+if [ -z "$item" ]
+then
+	echo "Spotlight refresh failed : no internet connection" | systemd-cat -t spotlight
+	exit 1
+fi
 landscapeUrl=$(echo $item | jq -r ".ad.image_fullscreen_001_landscape.u")
 title=$(echo $item | jq -r ".ad.title_text.tx")
 titleUrl=$(echo $item | jq -r ".ad.title_destination_url.u" | perl -pe 's/.*?(http.*)/\1/')
-
-mkdir -p "$HOME/.spotlight"
-path="$HOME/.spotlight/background.jpg"
+if [ ! -d $path ]
+then
+	mkdir -p $path
+fi
+slug=$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+path="$path$(date +"%Y%m%d%H%M")-$slug.jpg"
 	
 wget -qO "$path" "$landscapeUrl"
 
-gsettings set org.gnome.desktop.background picture-options "zoom"
-gsettings set org.gnome.desktop.background picture-uri "file://$path"
+gsettings set org.gnome.desktop.screensaver picture-options "zoom"
+gsettings set org.gnome.desktop.screensaver picture-uri "file://$path"
 
-notify-send "Background changed" "$title ($titleUrl)" --icon=preferences-desktop-wallpaper --urgency=low --hint=string:desktop-entry:spotlight
-echo "Background changed to $title ($titleUrl)" | systemd-cat -t spotlight
+notify-send "Screensaver changed" "$title ($titleUrl)" --icon=preferences-desktop-wallpaper
+echo "Screensaver changed to $title ($titleUrl)" | systemd-cat -t spotlight
