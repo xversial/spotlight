502be980c2a99fc3189c0d2db9f379681cdcec92
Rémy J <remy@remyj.fr>
2017-06-02 22:27:34 +0200

    Fix error while json array format was changed

--- spotlight.sh
+++ spotlight.sh
@@ -1,21 +1,18 @@
 #! /bin/bash
 
 path="$HOME/.spotlight/"
-item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=fr,fr-FR&ctry=FR" | jq -r ".batchrsp.items | .[0].item")
+item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=fr,fr-FR&ctry=FR" | jq -r ".batchrsp.items | .[0].item" | sed 's/var adData =//' | sed -e '/;/q' |sed -e 's/;//')
 if [ -z "$item" ]
 then
 	echo "Spotlight refresh failed : no internet connection" | systemd-cat -t spotlight
 	exit 1
 fi
 landscapeUrl=$(echo $item | jq -r ".ad.image_fullscreen_001_landscape.u")
-title=$(echo $item | jq -r ".ad.title_text.tx")
-titleUrl=$(echo $item | jq -r ".ad.title_destination_url.u" | perl -pe 's/.*?(http.*)/\1/')
 if [ ! -d $path ]
 then
 	mkdir -p $path
 fi
-slug=$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
-path="$path$(date +"%Y%m%d%H%M")-$slug.jpg"
+path="$path$(date +"%Y%m%d%H%M").jpg"
 	
 wget -qO "$path" "$landscapeUrl"
 
