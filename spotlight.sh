02ee8c4320b61ae2cb5c7f287d872fa441d606bc
Rémy J <remy@remyj.fr>
2017-08-15 14:57:30 +0200

    Re adding title and URL for notification

--- spotlight.sh
+++ spotlight.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-path="$HOME/.spotlight/"
+directory="$HOME/.spotlight/"
 item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=fr,fr-FR&ctry=FR" | jq -r ".batchrsp.items | .[0].item" | sed 's/var adData =//' | sed -e '/;/q' |sed -e 's/;//')
 if [ -z "$item" ]
 then
@@ -8,16 +8,20 @@ then
 	exit 1
 fi
 landscapeUrl=$(echo $item | jq -r ".ad.image_fullscreen_001_landscape.u")
+title=$(echo $item | jq -r ".ad.title_text.tx")
+titleUrl=$(echo $item | jq -r ".ad.title_destination_url.u" | perl -pe 's/.*?(http.*)/\1/')
+
 if [ ! -d $path ]
 then
 	mkdir -p $path
 fi
-path="$path$(date +"%Y%m%d%H%M").jpg"
+slug=$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+fullpath="$directory$(date +"%Y%m%d%H%M")-$slug.jpg"
 	
-wget -qO "$path" "$landscapeUrl"
+wget -qO "$fullpath" "$landscapeUrl"
 
 gsettings set org.gnome.desktop.screensaver picture-options "zoom"
-gsettings set org.gnome.desktop.screensaver picture-uri "file://$path"
+gsettings set org.gnome.desktop.screensaver picture-uri "file://$fullpath"
 
 notify-send "Screensaver changed" "$title ($titleUrl)" --icon=preferences-desktop-wallpaper
 echo "Screensaver changed to $title ($titleUrl)" | systemd-cat -t spotlight
