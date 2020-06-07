6cd01a818497a75633e4d0344a8724731c95b7ec
Rémy Jacquin <remy@remyj.fr>
2020-06-07 18:46:34 +0200

    Clean duplicate files

--- spotlight.sh
+++ spotlight.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-directory="$HOME/.spotlight/"
+directory="$HOME/.spotlight"
+
+# Get image info
 item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=fr,fr-FR&ctry=FR" | jq -r ".batchrsp.items | .[0].item" | sed 's/var adData =//' | sed -e '/;/q' |sed -e 's/;//')
 if [ -z "$item" ]
 then
@@ -15,15 +17,31 @@ if [ ! -d $directory ]
 then
 	mkdir -p $directory
 fi
-slug=$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
-fullpath="$directory$(date +"%Y%m%d%H%M")-$slug.jpg"
-	
+
+# file infos
+filename="$(date +"%Y%m%d%H%M")-$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z).jpg"
+fullpath="$directory/$filename"
+
+# Download image
 wget -qO "$fullpath" "$landscapeUrl"
 
+# Update background link with old screensaver image
 oldPicture=`readlink -f "$directory/.screensaver.jpg"`
 ln -sf $oldPicture "$directory/.background.jpg"
 
+# Update screensaver image
 ln -sf $fullpath "$directory/.screensaver.jpg"
+
+# Clean duplicates
+if [ ! -f "$directory/.md5sum" ]; then
+	touch "$directory/.md5sum"
+fi
+md5=$(md5sum $filename | awk '{print $1}')
+awk "/$md5/{print $2}" "$directory/.md5sum" | xargs /bin/rm -f
+sed -i "/$md5/d" "$directory/.md5sum"
+
+echo "$md5 $filename" >> "$directory/.md5sum"
 
 notify-send "Screensaver changed" "$title ($titleUrl)" --icon=preferences-desktop-wallpaper
 echo "Screensaver changed to $title ($titleUrl)" | systemd-cat -t spotlight
+
